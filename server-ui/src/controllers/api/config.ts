258c487cca4ed7e762f2af1dd05659f6078f5a35
wof4 <53194557+wof4@users.noreply.github.com>
2021-09-30 14:00:51 +0300

    Update config.ts

--- server-ui/src/controllers/api/config.ts
+++ server-ui/src/controllers/api/config.ts
@@ -5,7 +5,6 @@ const config = {
     Authorization: process.env.token,
 
     //в пустую строку нужно втавить кокен в формате 'Bearer хххххххххххх'
-    //Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjUzMTk0NTU3IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IndvZjQiLCJ1cm46Z2l0aHViOnVybCI6Imh0dHBzOi8vYXBpLmdpdGh1Yi5jb20vdXNlcnMvd29mNCIsIm5iZiI6MTYyNjI3MjY2OCwiZXhwIjoxNjI4ODY0NjY4LCJpc3MiOiJTaHJpLUlzc3Vlci1UZXN0IiwiYXVkIjoiU2hyaS1BdWRpZW5jZS1UZXN0In0.MlTIs3BSXo2Vsz2u74WnD73kW2qEpV44Y9dxy2UF-O0',
   },
 };
 
